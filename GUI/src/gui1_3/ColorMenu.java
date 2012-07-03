9c7f3a85bade01687aec22412ec5a0e6b15de4e6
Shinjiroh Hara <shinroh.hara@gmail.com>
2012-07-04 01:39:26 +0900

    ７月の課題

---
+++ GUI/src/gui1_3/ColorMenu.java
@@ -0,0 +1,56 @@
+package gui1_3;
+
+
+import java.awt.Color;
+import java.awt.Menu;
+import java.awt.MenuItem;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ColorMenu extends Menu implements ActionListener {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	private Type cuurentColor;
+	
+	enum Type {
+		
+		BLUE(Color.BLUE),
+		WHITE(Color.WHITE),
+		BLACK(Color.BLACK),
+		GREEN(Color.GREEN),
+		YELLOW(Color.YELLOW),
+		RED(Color.RED);
+		
+		
+		private Color color;
+		Type(Color color) {
+			this.color = color;
+		}
+		public Color getColor() {
+			return color;
+		}
+	}
+	
+	public ColorMenu(String menuName, Type color) {
+		super(menuName);
+		cuurentColor = color;
+		for (Type element: Type.values()) {
+			MenuItem mi = new MenuItem(element.toString());
+			add(mi);
+			mi.addActionListener(this);
+		}
+	}
+	
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		cuurentColor = Type.valueOf(e.getActionCommand());
+	}
+	
+	public Type getColor() {
+		return cuurentColor;
+	}
+}
--- GUI/src/gui1_3/Display.java
+++ GUI/src/gui1_3/Display.java
@@ -1,27 +1,27 @@
 package gui1_3;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.font.FontRenderContext;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class Display extends Frame implements ActionListener {
+public class Display extends Window implements MouseMotionListener, MouseListener{
 
 	private static final long serialVersionUID = -6817980970800362509L;
 	
+	
 
 	
-	private Property property = new Property(this);
+	private Property property = new Property();
 	
 	int cuurentSize = 64;
-	private Font currentFont = new Font(Font.SANS_SERIF, Font.PLAIN, cuurentSize);
-	private Color currentColor = Color.RED;
 	
-	
-	
+	private Point pressedPoint;
 	
 	public static void main (String args[]) {
 		Display watch = new Display();
@@ -36,17 +36,12 @@ public class Display extends Frame implements ActionListener {
 		}
 	}
 	Display() {
-		super("DigitalWatch");
-
+		super(new Frame("DegitalWatch"));
 		setSize(400, 200);
 		setVisible(true);
-		MenuBar mb = new MenuBar();
-		setMenuBar(mb);
-		Menu menuDisp = new Menu("•\Ž¦");
-		mb.add(menuDisp);
-		MenuItem itemProp = new MenuItem(Property.MENU_NAME);
-		menuDisp.add(itemProp);
-		itemProp.addActionListener(property);
+		add(property);
+		addMouseMotionListener(this);
+		addMouseListener(this);
 		setBackground(Color.WHITE);
 		
 		addWindowListener(new WindowAdapter(){
@@ -56,18 +51,15 @@ public class Display extends Frame implements ActionListener {
 		});
 	}
 	
-	
-	public void actionPerformed(ActionEvent e) {
-		property.setVisible(true);
-	}
 
-	
 	public void paint(Graphics g) {
-		
-		
 		Dimension dim = getSize();
-		double width =  dim.getWidth();
-		double height = dim.getHeight();
+		int size = property.getCurrentFont().getSize();
+		double width =  size*5;
+		double height = size+2.5 + 20;
+		setSize((int)width, (int)height);
+		setBackground(property.getBackGround().getColor());
+		
 		
 		Date date = new Date();
 		SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss");
@@ -75,20 +67,62 @@ public class Display extends Frame implements ActionListener {
 		Image back = createImage((int)width, (int)height);
 		Graphics buffer = back.getGraphics();
 	
-		buffer.setFont(currentFont);
-		buffer.setColor(currentColor);
+		buffer.setFont(property.getCurrentFont());
+		buffer.setColor(property.getCurrentColor().getColor());
 		buffer.drawString(sdf.format(date), (int)width/15, (int)height/2+20);
 		g.drawImage(back, 0, 0, this);
+		
+		
+
+		
 	}
 	
 	public void update(Graphics g) {
 		paint(g);
 	}
-	public void setStringColor(Color color) {
-		currentColor = color;
+	
+	
+	
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		if (pressedPoint == null) {
+			throw new IllegalStateException("Mouse is not pressed");
+		}
+		Point current = getLocation();
+		current.translate(e.getPoint().x - pressedPoint.x, e.getPoint().y - pressedPoint.y);
+		setLocation(current);
+	}
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		
+	}
+	@Override
+	public void mouseClicked(MouseEvent me) {
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		
 	}
 	
-	public void setStringFont(Font font) {
-		currentFont = font;
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		
+	}
+	
+	@Override
+	public void mousePressed(MouseEvent e) {
+		if (e.isPopupTrigger()) {
+			property.show(e.getComponent(), e.getX(), e.getY());
+		} else if (e.getButton() == MouseEvent.BUTTON1) {
+			pressedPoint = e.getPoint();
+		}
+	}
+	
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		if (e.isPopupTrigger()) {
+			property.show(e.getComponent(), e.getX(), e.getY());
+		}
 	}
 }
